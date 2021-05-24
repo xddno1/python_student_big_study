e046a6584320590f94a44df9dcf734410f6b09ff
xddno1 <2196815716@qq.com>
2021-05-24 20:10:23 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from openpyxl import load_workbook
+input("请将 ”1.xlsx“ 与本脚本放在同一个文件夹中（回车键继续）")
+wb = load_workbook('1.xlsx')
+sheets = wb.worksheets  # 获取当前所有的sheet
+myclassstudent = ['陈荣森', '邓京锐', '邓文凯', '何江伟', '何锦胜', '李春江', '李锦科', '廖金威', '廖钧濠', '林荣添', '刘继洪', '罗炜芊', '麦洋华', '彭浩林', '唐爱萍', '涂骏', '冼东潮', '肖华锋', '谢泽琛', '杨奋发', '张杰森', '郑佳浩', '植美麟', '周天宝']
+# 24团员
+# print(len(myclassstudent))
+# 获取第一张sheet
+sheet1 = sheets[0]
+studyedstudent = []
+for col in sheet1['A']:
+    studyedstudent.append(col.value)
+# print(studyedstudent)
+
+unstudyedstudent = []
+for i in myclassstudent:
+    if i not in studyedstudent:
+        unstudyedstudent.append(i)
+file = open('大学习未完成名单.txt', 'w')
+
+for i in unstudyedstudent:
+    file.write(i)
+    file.write('\n')
+
+
+
