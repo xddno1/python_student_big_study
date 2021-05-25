418de6fd0fdf284ecd10b0e02a4ff737cbe30e4b
xddno1 <2196815716@qq.com>
2021-05-25 13:19:32 +0800

    1

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # python_student_big_study
 主要是根目录下的main.py，其功能是统计xdd班上的未完成青年大学习的同学的名单
 
+根目录下的ui.py，其功能是一个简单展示的界面
+
 该项目用到的技术栈：
+
+- python的模块化
+- pyqt5做一个简单的界面
+
 - openpyxl库对xlsx文件进行操作
-- Pyinstaller库对main.py文件进行打包
-- Pyinstaller -F -i head.ico main.py语句打包添加图像
+- Pyinstaller库对ui.py文件进行打包
+- Pyinstaller -F -w ui.py 语句打包
+
---
+++ ui.py
@@ -0,0 +1,47 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QTextEdit, QFileDialog, QApplication, QHBoxLayout, QVBoxLayout, QPushButton)
+from PyQt5.QtGui import QIcon
+import main
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        okButton = QPushButton("选择文件")
+        self.textEdit = QTextEdit()
+        okButton.clicked.connect(self.showDialog)
+        vvbox = QVBoxLayout()
+        vvbox.addWidget(okButton)
+        vvbox.addStretch(1)
+        hbox = QHBoxLayout()
+        hbox.addLayout(vvbox)
+        hbox.addWidget(self.textEdit)
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+        self.setGeometry(300, 300, 350, 300)
+        self.setWindowTitle("青年大学习获取未学习名单")
+        self.setWindowIcon(QIcon("head.ico"))
+        self.show()
+
+    def showDialog(self):
+        # 弹出文件选择器
+        fname = QFileDialog.getOpenFileName(self, "Open file")
+        # 如果选择了文件
+        if fname[0]:
+            # 打开第一个文件
+            f = open(fname[0], "r")
+            print(f.name)
+            mylist = main.getnostudytxt(f.name)
+            print(mylist)
+            for i in mylist:
+                self.textEdit.append(i)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
